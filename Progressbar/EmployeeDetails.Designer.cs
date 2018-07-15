7df675a04d82b5b0d44a08c25fa27d4fbdea6dc9
tvrajah <tvrajah@gmail.com>
2018-07-15 12:51:52 +0800

    Initial commit

---
+++ Progressbar/EmployeeDetails.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Progressbar
+{
+    partial class EmployeeDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.bckgndwrkr = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 71);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(473, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // bckgndwrkr
+            // 
+            this.bckgndwrkr.WorkerReportsProgress = true;
+            this.bckgndwrkr.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bckgndwrkr_DoWork);
+            this.bckgndwrkr.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bckgndwrkr_ProgressChanged);
+            // 
+            // EmployeeDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 406);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "EmployeeDetails";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.EmployeeDetails_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.ComponentModel.BackgroundWorker bckgndwrkr;
+    }
+}
+
---
+++ Progressbar/EmployeeDetails.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading;
+
+namespace Progressbar
+{
+    public partial class EmployeeDetails : Form
+    {
+        public EmployeeDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void EmployeeDetails_Load(object sender, EventArgs e)
+        {
+            // Start the BackgroundWorker.
+            bckgndwrkr.RunWorkerAsync();
+        }
+
+        private void bckgndwrkr_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                // Wait 100 milliseconds.
+                Thread.Sleep(100);
+                // Report progress.
+                bckgndwrkr.ReportProgress(i);
+            }
+        }
+
+        private void bckgndwrkr_ProgressChanged(object sender,
+            ProgressChangedEventArgs e)
+        {
+            // Change the value of the ProgressBar to the BackgroundWorker progress.
+            progressBar1.Value = e.ProgressPercentage;
+            // Set the text.
+            this.Text = e.ProgressPercentage.ToString();
+        }
+    }
+}
